8a4807c40d05c9877ec8cdac4ae0e6fa2dee9dfc
CLEBER JOSE LEAO DE ALMEIDA <cleber.almeida@ulife.com.br>
2025-04-07 19:51:18 -0300

    add exe-1

---
+++ exe-1/TipoVariavel.java
@@ -0,0 +1,18 @@
+public class TipoVariavel {
+    public static void main(String[] args) {
+        int a;
+        double b;
+        String c;
+        boolean d;
+
+        a = 7;
+        b = 5.23;
+        c = "Boa noite";
+        d = true;
+
+        System.out.println("Este é meu número Inteiro: "+ a);
+        System.out.println("Este é meu número  Real: "+ b);
+        System.out.println("Este é meu número caractere: "+ c);
+        System.out.println("Este é meu número booleado: "+ d);
+    }
+}
